a004e07fccbfc860be8491aeac17ff15bb432af2
CrisMonsalve348 <davidalzate453@gamil.com>
2026-04-25 11:58:44 -0500

    implementacion de login y registro basico, falta middleware para registro

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['worker', 'admin'])->default('worker');
             $table->timestamp('email_verified_at')->nullable();
+            $table->integer('phone-number')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
